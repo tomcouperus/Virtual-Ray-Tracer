40356db7e4071c7ee3487225376faf55e29cbbee
tom couperus <tcouperus@hotmail.nl>
2023-05-12 16:14:54 +0200

    Remove click functionality from vertices

--- Unity/Assets/_Project/Texturer/InteractableMesh.cs
+++ Unity/Assets/_Project/Texturer/InteractableMesh.cs
@@ -6,15 +6,19 @@ using UnityEngine;
 public class InteractableMesh : MonoBehaviour
 {
 
-    public ClickableVertex clickableVertexPrefab;
+    public Vertex vertexPrefab;
     // Start is called before the first frame update
     void Start()
     {
+        GenerateVertices();
+    }
+
+    void GenerateVertices() {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         HashSet<Vector3> vertices = new HashSet<Vector3>(mesh.vertices);
         foreach(Vector3 vertex in vertices) {
-            ClickableVertex clickVert = Instantiate(clickableVertexPrefab, transform);
-            clickVert.SetPosition(vertex);
+            Vertex vert = Instantiate(vertexPrefab, transform);
+            vert.SetPosition(vertex);
         }
     }
 }
--- Unity/Assets/_Project/Texturer/Vertex.cs
+++ Unity/Assets/_Project/Texturer/Vertex.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickableVertex : MonoBehaviour {
+public class Vertex : MonoBehaviour {
 
     public void SetPosition(Vector3 position) {
         transform.position = position + transform.parent.position;
